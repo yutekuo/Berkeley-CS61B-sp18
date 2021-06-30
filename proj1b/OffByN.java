98ad75f0979440c7d1141c604f37900b6fffc530
Yu Te Kuo <f04506081@gs.ncku.edu.tw>
2021-07-01 00:43:24 +0800

    Completeed Task 5: OffByN

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,2 +1,13 @@
-public class OffByN {
+public class OffByN implements CharacterComparator {
+    private int difference;
+    /** A single argument constructor which takes an integer. */
+    public OffByN(int N) {
+        difference = N;
+    }
+
+    /** Returns true for characters that are off by N. */
+    @Override
+    public boolean equalChars(char x, char y) {
+        return x - y == difference || x - y == -difference;
+    }
 }
