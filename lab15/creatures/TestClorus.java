c9988b75274806426af0ceae58006ca4f5066d13
Yu Te Kuo <f04506081@gs.ncku.edu.tw>
2021-07-03 15:05:33 +0800

    Fix style check issue

--- lab15/creatures/TestClorus.java
+++ lab15/creatures/TestClorus.java
@@ -2,13 +2,11 @@ package creatures;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.HashMap;
-import java.awt.Color;
 import huglife.Direction;
 import huglife.Action;
 import huglife.Occupant;
 import huglife.Impassible;
 import huglife.Empty;
-import creatures.Plip;
 
 public class TestClorus {
     @Test
