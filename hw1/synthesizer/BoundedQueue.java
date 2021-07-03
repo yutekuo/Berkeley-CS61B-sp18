19ed2733ef6d2ff17a34f9dcb287bf9114558e36
Yu Te Kuo <f04506081@gs.ncku.edu.tw>
2021-07-03 21:04:59 +0800

    Completed Task 1: BoundedQueue

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,28 @@
+package synthesizer;
+
+public interface BoundedQueue<T> {
+    /** Returns size of the buffer. */
+    int capacity();
+
+    /** Returns number of items currently in the buffer. */
+    int fillCount();
+
+    /** Adds item x to the end. */
+    void enqueue(T x);
+
+    /** Deletes and returns item from the front. */
+    T dequeue();
+
+    /** Returns (but do not deletes) item from the front. */
+    T peek();
+
+    /** Is the buffer empty (fillCount equals zero)? */
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }
+
+    /** Is the buffer full (fillCount is same as capacity)? */
+    default boolean isFull() {
+        return fillCount() == capacity();
+    }
+}
