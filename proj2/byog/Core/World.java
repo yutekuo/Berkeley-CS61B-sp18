7cf28729695aa03de96bd4f5680b14a72ab1d3e4
Yu Te Kuo <f04506081@gs.ncku.edu.tw>
2021-07-06 23:21:24 +0800

    Added overlap checking to addOneRoom method

--- proj2/byog/Core/World.java
+++ proj2/byog/Core/World.java
@@ -25,6 +25,8 @@ public class World {
     /**
      * Adds a rectangle room with random width and height to a random location in the world.
      *
+     * @return the room that is added to the world.
+     *
      * A room should have both width and height greater than or equal to 4 and smaller than
      * or equal to roomMaxSize.
      * e.g., the smallest room. ('#' means wall and '.' means floor)
@@ -34,15 +36,94 @@ public class World {
      *      ####
      */
     public void addOneRoom() {
+        Room room = roomGenerator();
+        existingRooms.add(room);
+        createOneRoom(room.getWidth(), room.getHeight(), room.getPosition());
+    }
+
+    /** Returns a room which doesn't overlap any room in the existing room list. */
+    private Room roomGenerator() {
         Position roomPosition = getRandomPosition();
         int roomWidth = getRandomRoomWidth(width - roomPosition.getX());
         int roomHeight = getRandomRoomHeight(height - roomPosition.getY());
         Room room = new Room(roomWidth, roomHeight, roomPosition);
-        existingRooms.add(room);
-        createOneRoom(roomWidth, roomHeight, roomPosition);
+        if (isOverlap(existingRooms, room)) {
+            return roomGenerator();
+        }
+        return room;
     }
 
-    /** Create one room in the world. */
+    /** Returns true if a room overlaps any room in roomList, false otherwise. */
+    private static boolean isOverlap(ArrayList<Room> roomList, Room room) {
+        for (Room r : roomList) {
+            if (isNewRoomInOldRoom(r, room)) {
+                return true;
+            }
+
+            if (isOldRoomInNewRoom(r, room)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns true if r2's bottom-left position is included in r1, false otherwise.
+     * @param r1 the existing room.
+     * @param r2 the new room.
+     */
+    private static boolean isNewRoomInOldRoom(Room r1, Room r2) {
+        int r1X = r1.getPosition().getX();
+        int r1Y = r1.getPosition().getY();
+        int r2X = r2.getPosition().getX();
+        int r2Y = r2.getPosition().getY();
+
+        int dx = r2X - r1X;
+        if (dx < 0 || dx > r1.getWidth()) {
+            return false;
+        }
+
+        int dy = r2Y - r1Y;
+        if (dy < 0 || dy > r1.getHeight()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns true if r1's bottom-left position is included in r2, false otherwise.
+     * @param r1 the existing room.
+     * @param r2 the new room.
+     */
+    private static boolean isOldRoomInNewRoom(Room r1, Room r2) {
+        int r1X = r1.getPosition().getX();
+        int r1Y = r1.getPosition().getY();
+        int r2X = r2.getPosition().getX();
+        int r2Y = r2.getPosition().getY();
+
+        if (r2X >= r1X && r2X <= r1X + r1.getWidth() - 1 && r2Y < r1Y) {
+            if (r2Y + r2.getHeight() - 1 >= r1Y) {
+                return true;
+            }
+        }
+
+        if (r2X < r1X && r2Y < r1Y) {
+            if (r2X + r2.getHeight() - 1 >= r1X && r2Y + r2.getHeight() - 1 >= r1Y) {
+                return true;
+            }
+        }
+
+        if (r2X < r1X && r2Y <= r1Y + r1.getHeight() - 1 && r2Y >= r1Y) {
+            if (r2X + r2.getWidth() - 1 >= r1X) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /** Creates one room in the world. */
     private void createOneRoom(int roomWidth, int roomHeight, Position roomPosition) {
         // Fill in floors.
         for (int x = 1; x < roomWidth - 1; x++) {
