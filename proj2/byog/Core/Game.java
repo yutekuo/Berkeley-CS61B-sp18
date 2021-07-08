669ed4e997bb376fc76018b362cf62e2c40b0cbc
Yu Te Kuo <f04506081@gs.ncku.edu.tw>
2021-07-08 13:02:49 +0800

    Comment ter.initialize() to pass autograder

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -9,9 +9,9 @@ public class Game {
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
 
-    public Game() {
+/*    public Game() {
         ter.initialize(WIDTH, HEIGHT);
-    }
+    }*/
 
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
@@ -42,7 +42,7 @@ public class Game {
         worldMap.addDoor();
 
         // Draw the world on the screen.
-        ter.renderFrame(worldMap.getWorld());
+        //ter.renderFrame(worldMap.getWorld());
         return worldMap.getWorld();
     }
 }
