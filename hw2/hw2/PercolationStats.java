5e94ae78a98bea7a3851df7cc525ca24a7fe8fbc
Yu Te Kuo <f04506081@gs.ncku.edu.tw>
2021-07-21 16:41:02 +0800

    Completed PercolationStats

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,28 +1,59 @@
 package hw2;
 
-public class PercolationStats {
-    /** Perform T independent experiments on an N-by-N grid. */
-    public PercolationStats(int N, int T, PercolationFactory pf) {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import static java.lang.Math.sqrt;
 
+public class PercolationStats {
+    private double[] threshold;
+    /**
+     * Perform T independent experiments on an N-by-N grid.
+     * To estimate the percolation threshold, consider the following computational experiment:
+     * 1. Initialize all sites to be blocked.
+     * 2. Repeat the following until the system percolates:
+     *    2-1. Choose a site uniformly at random among all blocked sites.
+     *    2-2. Open the site.
+     *    2-3. The fraction of sites that are opened when the system percolates provides
+     *         an estimate of the percolation threshold.
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N and T must be greater than zero.");
+        }
+        threshold = new double[T];
+        Percolation p = pf.make(N);
+        int count = 0;
+        while (count < T) {
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(0, N - 1);
+                int col = StdRandom.uniform(0, N - 1);
+                if (p.isOpen(row, col)) {
+                    continue;
+                }
+                p.open(row, col);
+            }
+            threshold[count] = (double) p.numberOfOpenSites() / (N * N);
+            count++;
+        }
     }
 
     /** Sample mean of percolation threshold. */
     public double mean() {
-        return 0;
+        return StdStats.mean(threshold);
     }
 
     /** Sample standard deviation of percolation threshold. */
     public double stddev() {
-        return 0;
+        return StdStats.stddev(threshold);
     }
 
     /** Low endpoint of 95% confidence interval. */
     public double confidenceLow() {
-        return 0;
+        return mean() - (1.96 * stddev() / sqrt(threshold.length));
     }
 
     /** High endpoint of 95% confidence interval. */
     public double confidenceHigh() {
-        return 0;
+        return mean() + (1.96 * stddev() / sqrt(threshold.length));
     }
 }
