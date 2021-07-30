2e32c689f14c166f3da2595b2c52d1d51b26b8f1
Yu Te Kuo <f04506081@gs.ncku.edu.tw>
2021-07-30 15:46:47 +0800

    Completed MyHashMap

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -7,7 +7,7 @@ import java.util.Set;
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
- *  @author Your name here
+ *  @author Yu Te Kuo
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
@@ -53,19 +53,24 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        int bucketIndex = hash(key);
+        return buckets[bucketIndex].get(key);
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        int bucketIndex = hash(key);
+        if (!containsKey(key)) {
+            size++;
+        }
+        buckets[bucketIndex].put(key, value);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
