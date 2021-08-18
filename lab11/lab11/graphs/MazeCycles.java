9c5ed69500ac5a9d8451bed45e2b949a32ef42be
Yu-Te Kuo <f04056081@gs.ncku.edu.tw>
2021-08-18 13:45:34 -0500

    Completed lab11

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,7 @@
 package lab11.graphs;
 
 /**
- *  @author Josh Hug
+ *  @author Yu-Te Kuo
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,16 +9,54 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int s;
+    private boolean isCycleExist = false;
+    private int[] parent;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(1, 1);
+        distTo[s] = 0;
+        parent = new int[maze.V()];
+        for (int i = 0; i < maze.V(); i++) {
+            parent[i] = i;
+        }
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
     }
 
-    // Helper methods go here
+    private void dfs(int v) {
+        if (isCycleExist) {
+            return;
+        }
+
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                distTo[w] = distTo[v] + 1;
+                parent[w] = v;
+                dfs(w);
+            }
+
+            if (parent[v] != w) {
+                isCycleExist = true;
+                int u = v;
+                while (u != w) {
+                    edgeTo[u] = parent[u];
+                    u = parent[u];
+                }
+                edgeTo[w] =v;
+                announce();
+                return;
+            }
+        }
+    }
 }
 
